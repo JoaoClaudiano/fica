b693f6406a7769089ce2a6a63765024f83e738fe
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2026-01-20 19:32:47 -0300

    Atualizar o components.js

--- public/scripts/components.js
+++ public/scripts/components.js
@@ -1,9 +1,13 @@
-// Carregar um componente HTML
-export function loadComponent(containerId, url){
-  const container = document.getElementById(containerId);
-  if(container){
-    fetch(url)
-      .then(r => r.text())
-      .then(html => container.innerHTML = html);
-  }
+async function loadComponent(selector, path) {
+  const container = document.querySelector(selector);
+  if (!container) return;
+
+  const res = await fetch(path);
+  const html = await res.text();
+  container.innerHTML = html;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadComponent("#header", "/fica/public/components/header.html");
+  loadComponent("#footer", "/fica/public/components/footer.html");
+});
