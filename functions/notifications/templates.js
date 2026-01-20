9706d2a7da42c474fdd108c0a75fce001c6a2f5b
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2026-01-20 19:14:10 -0300

    Criar templates.js

---
+++ functions/notifications/templates.js
@@ -0,0 +1,5 @@
+module.exports = {
+  low: "Estamos aqui para te apoiar. Conte com a escola.",
+  moderate: "Percebemos alguns sinais de atenção. Vamos conversar?",
+  high: "Você não está sozinho. A escola está disponível para te ajudar."
+};
